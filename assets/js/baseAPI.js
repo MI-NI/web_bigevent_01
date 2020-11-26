7e6f49e7a8972403fcb93b92c77d9a0d62e83821
saibo <saibo2608176664@qq.com>
2020-11-26 10:38:04 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,6 +2,22 @@
 // 会先调用ajaxPrefilter这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function(options) {
-    // 再发起正真的Ajax请求之前，统一拼接·的跟陆劲
+    // 1.再发起正真的Ajax请求之前，统一拼接·的跟陆劲
     options.url = "http://ajax.frontend.itheima.net" + options.url
+        // 2.对需要权限的接口配置头信息
+        // 必须my开头才行
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ""
+        }
+    }
+    // 3.登录拦截
+    // 发送请求，无论失败成功都会有一次回调函数--complete
+    options.complete = function(res) {
+        // 请求失败，而且是是否认证失败，就进行强制跳转和销毁token
+        if (res.responseJSON.status !== 0 && res.responseJSON.message === "身份认证失败！") {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,56 @@
+$(function() {
+        // 1.获取渲染
+        getuserInof();
+        // 2.点击退出
+        var layer = layui.layer
+        $('#btnLogout').on('click', function() {
+                layer.confirm('确定是否退出登录?', { icon: 3, title: '提示' }, function(index) {
+                    //do something
+                    // 清空本地存储
+                    localStorage.removeItem('token')
+                        // 跳转到login,html
+                    location.href = "/login.html"
+                        // 关闭询问框
+                    layer.close(index);
+                });
+            })
+            // 3登录拦截，优化到了BaseApi.js
+
+    })
+    // 封装一个获取信息的函数
+    // 一定要写在入口函数外面，因为别的页面也要调用，写入入口函数就是局部函数
+function getuserInof() {
+    // 发送Ajax
+    $.ajax({
+        method: "GET",
+        url: "/my/userinfo",
+        // headers,请求头---这里写请求头是因为，这里右权限，看说明文档
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ""
+        // },
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message)
+            }
+            // 请求成功。渲染用户头像信息
+            renderAvatar(res.data)
+        }
+    })
+}
+// 渲染项和名字
+function renderAvatar(user) {
+    // 1.用户名，有昵称就显示，没有就显示username
+    var name = user.nickname || user.username
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+        // 2.开始渲染头像
+    if (user.user_pic !== null) {
+        // 有头像
+        $('.layui-nav-img').show().attr('src', user.user_pic)
+        $('.text-avatar').hide()
+    } else {
+        // 没有头像
+        var text = name[0].toUpperCase()
+        $('.text-avatar').show().html(text)
+        $('.layui-nav-img').hide()
+    }
+}
